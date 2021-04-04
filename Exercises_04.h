07ef21419389519fe60936550a424ad04e709edd
chamauta <chitoamauta@outlook.com>
2021-04-04 17:34:18 -0400

    Update Exercises_04.h

--- Exercises_04.h
+++ Exercises_04.h
@@ -7,26 +7,26 @@
 
 //================================================= function prototypes =========================================//
 
-void gasMileage();												//  4.13
-void largest();														//  4.17
-void twoLargest(); 												//  4.19
-void pattern1();													//  4.21
-void pattern2();													//  4.22
-void danglingElse1();											//  4.23a
-void danglingElse2();											//  4.23b
-void danglingElse3();											//  4.24a
-void danglingElse4();											//  4.24b
-void danglingElse5();											//  4.24c
-void danglingElse6();											//  4.24d
-void squareOfAsterisks();									//  4.25
-void palindrome();												//  4.26
-double reversedOrder(int);								//  related to 4.26 not an exercise in chapter 4
-void binToDec();													//  4.27
-int decToBin(int);												//  related to 4.27 not an exercise in chapter 4
-void checkboard();												//  4.28
-void circle();														//  4.30
-void anyTriangle();												//  4.32
-void rightTriangle();											//  4.33
-double factorial(double);									//  4.34a
-double e(int);														//  4.34b
-double eX(double);												//  4.34c
+void gasMileage();																	//  4.13
+void largest();																			//  4.17
+void twoLargest();																	//  4.19
+void pattern1();																		//  4.21
+void pattern2();																		//  4.22
+void danglingElse1();//  4.23a
+void danglingElse2();//  4.23b
+void danglingElse3();//  4.24a
+void danglingElse4();//  4.24b
+void danglingElse5();//  4.24c
+void danglingElse6();//  4.24d
+void squareOfAsterisks();//  4.25
+void palindrome();//  4.26
+double reversedOrder(int);//  related to 4.26 not an exercise in chapter 4
+void binToDec();//  4.27
+int decToBin(int);//  related to 4.27 not an exercise in chapter 4
+void checkboard();//  4.28
+void circle();//  4.30
+void anyTriangle();//  4.32
+void rightTriangle();//  4.33
+double factorial(double);//  4.34a
+double e(int);//  4.34b
+double eX(double);//  4.34c
